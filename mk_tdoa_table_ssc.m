b7e27df4495af02a5bb2e6fa4e499fc3c0d9a27a
Taewoo Lee <twlee@speech.korea.ac.kr>
2015-05-16 15:59:00 +0900

    Update mk_tdoa_table_ssc.m

--- mk_tdoa_table_ssc.m
+++ mk_tdoa_table_ssc.m
@@ -8,11 +8,24 @@ function []=mk_tdoa_table_ssc(tdoa_table_filename)
 % IEEE Transactions on Consumer Electronics, vol. 55, no. 3, pp. 1663-1668, 
 % Aug. 2009.
 %
-% Author: Taewoo Lee (twlee@speech.korea.ac.kr). 2015.4.28.
+
+% Release date: May 2015
+% Author: Taewoo Lee, (twlee@speech.korea.ac.kr)
 %
-% This work is licensed under a Creative Commons Attribution 4.0
-% International License. See: http://creativecommons.org/licenses/by/4.0/.
+% Copyright (C) 2015 Taewoo Lee
 % 
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 % For all microphone pairs, the coordinates with a same TDOA are annotated 
 % as a same cluster ID. Then, it is saved into the CoordClusterTable.
