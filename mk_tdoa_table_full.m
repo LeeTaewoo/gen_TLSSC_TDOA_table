0e28dc14b2f86e2140b7a5cff70c76c61ab89b5f
Taewoo Lee <twlee@speech.korea.ac.kr>
2015-05-26 23:31:39 +0900

    Update mk_tdoa_table_full.m

--- mk_tdoa_table_full.m
+++ mk_tdoa_table_full.m
@@ -1,4 +1,4 @@
-function []=mk_tdoa_table_full(mic,search_range,Fs,c)
+function []=mk_tdoa_table_full(mic,mic_array_origin,search_range,Fs,c)
 % In : mic: cartesian coordinates of microphones 
 %      search_range: e.g.) theta=0:2*pi/360:2*pi, phi=0:pi/90:pi, r=2m.
 %      Fs: sampling frequency (Hz).
@@ -36,18 +36,17 @@ M= size(mic,1); % number of microphones
 N= M*(M-1)/2; % number of microphone pairs
 
 fprintf('TDOA Table Generation for Full-search\n');
-n_theta= search_range.n_theta;
-n_phi= search_range.n_phi;
-theta= linspace(0, search_range.theta_range, n_theta);
-phi= linspace(0, search_range.phi_range, n_phi); 
-cartCoords= zeros(1*n_theta*n_phi, 3);
+n_theta= size(search_range.theta,2);
+n_phi= size(search_range.phi,2);
+n_r= size(search_range.r,2);
+cartCoords= zeros(n_theta*n_phi*n_r, 3);
 coordCnt= 0;
 for r=search_range.r
-    for ti=1:n_theta
-        for pi=1:n_phi;
-            [x(1),x(2),x(3)]= sph2cart(theta(ti),phi(pi),r);
+    for ti=search_range.theta
+        for pi=search_range.phi;
+            [x(1),x(2),x(3)]= sph2cart(deg2rad(ti),deg2rad(pi),r);
             coordCnt= coordCnt+1;
-            cartCoords(coordCnt,:)= x';
+            cartCoords(coordCnt,:)= x + mic_array_origin;
         end
     end
 end
